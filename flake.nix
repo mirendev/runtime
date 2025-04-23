dab800244b233537445b396169654e7129496476
Paul Hinze <phinze@phinze.com>
2025-04-23 17:34:30 -0500

    Add a simple Nix flake w/ Go and Dagger
    The direnv part only triggers if nix is installed, so should be a noop for others

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A development shell for Miren Runtime";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.dagger.url = "github:dagger/nix";
+  inputs.dagger.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    dagger,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go_1_24
+            dagger.packages.${system}.dagger
+          ];
+        };
+      }
+    );
+}
