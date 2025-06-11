240ab2f4ec78fd03a25c2de58e9936a13a8e390b
Teresa Chu <teresa@miren.dev>
2025-06-11 13:58:01 -0400

    Host should be set on default application if no app argument is provided.

--- cli/commands/set_host.go
+++ cli/commands/set_host.go
@@ -1,19 +1,29 @@
 package commands
 
 import (
+	"fmt"
 	"miren.dev/runtime/api/app"
+	"miren.dev/runtime/api/core/core_v1alpha"
+	"miren.dev/runtime/api/entityserver/entityserver_v1alpha"
+	"miren.dev/runtime/pkg/entity"
 	"miren.dev/runtime/pkg/rpc"
+	"net"
 )
 
 func SetHost(ctx *Context, opts struct {
 	ConfigCentric
-	App  string `short:"a" long:"app" description:"Application name" required:"true"`
+	App  string `short:"a" long:"app" description:"Application name" required:"false"`
 	Host string `short:"h" long:"host" description:"Set host" required:"true"`
 }) error {
 	var (
 		rs     *rpc.State
 		client *rpc.NetworkClient
 	)
+	_, err := net.LookupIP(opts.Host)
+	if err != nil {
+		ctx.Printf("Warning: DNS lookup failed for host %q: %v\n", opts.Host, err)
+		return err
+	}
 
 	cc, err := opts.LoadConfig()
 	if err != nil {
@@ -40,14 +50,39 @@ func SetHost(ctx *Context, opts struct {
 		}
 	}
 
-	// Create app client
+	appName := opts.App
+
+	if appName == "" {
+		var app core_v1alpha.App
+		eac := entityserver_v1alpha.NewEntityAccessClient(client)
+		// Find app marked as default
+		resp, err := eac.List(ctx, entity.Bool(core_v1alpha.AppDefaultId, true))
+		if err != nil {
+			return fmt.Errorf("failed to list default app: %w", err)
+		}
+
+		if len(resp.Values()) == 0 {
+			return fmt.Errorf("No default app is currently set. Use app default set <app>.")
+			return nil
+		}
+
+		for _, ent := range resp.Values() {
+			app.Decode(ent.Entity())
+
+			var metadata core_v1alpha.Metadata
+			metadata.Decode(ent.Entity())
+			appName = metadata.Name
+		}
+		ctx.Printf("Using default app: %s\n", appName)
+
+	}
+
 	appClient, err := app.NewClient(ctx, ctx.Log, client)
 	if err != nil {
 		return err
 	}
 
-	// Set the host for the app
-	err = appClient.SetHost(ctx, opts.App, opts.Host)
+	err = appClient.SetHost(ctx, appName, opts.Host)
 	if err != nil {
 		return err
 	}
